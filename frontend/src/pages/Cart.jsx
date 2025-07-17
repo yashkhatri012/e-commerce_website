2885fcda1b2c096d75cfca93a78faa879e29875b
Yash <yashkhatri88540@gmail.com>
2025-07-17 16:35:18 +0530

    bug fixed 2

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -53,13 +53,13 @@ const Cart = () => {
 
                   <div>
                     <p className='text-xs sm:text-lg font-medium '>  {productData.name}</p>
-                    <div className='flex items-center gap-5 mt-2'>
-                        <p>{currency}{productData.price} </p>
+                    <div className='flex items-center gap-5 mt-2 text-sm'>
+                        <p className=''>{currency} {productData.price} </p>
                         <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50' >{item.size} </p>
                     </div>
                   </div>
               </div>
-              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size , Number(e.target.value))} className='border border-gray-300 rounded max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size , Number(e.target.value))} className='border border-gray-300 rounded max-w-6 sm:max-w-20 px-1 sm:px-2 py-1 text-sm' type="number" min={1} defaultValue={item.quantity} />
               <img  onClick={()=> updateQuantity(item._id, item.size, 0)}  className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
                 </div>
             )
